99e248abc7f36a26c98b0e89d1417045c4677116
rishabh-ioanyt <rishabh.maurya@ioanyt.com>
2022-12-13 17:59:00 +0530

    addByObject in WishlistProductService added

--- Local-B2C/src/main/java/com/b2c/Local/B2C/common/service/WishlistProductService.java
+++ Local-B2C/src/main/java/com/b2c/Local/B2C/common/service/WishlistProductService.java
@@ -65,41 +65,83 @@ public class WishlistProductService {
         }
         WishlistProduct wishlistProduct = new WishlistProduct();
         if (objectMapper.convertValue(wishlist, Laptop.class).getLaptopId() != null) {
-            wishlistProduct.setProduct(ProductEnum.LAPTOP);
-            wishlistProduct.setProductId(objectMapper.convertValue(wishlist, Laptop.class).getLaptopId());
-            wishlistProduct.setUser(getLoggedInUser());
-            wishlistProductRepository.save(wishlistProduct);
-            return wishlistProduct;
+            if (!laptopRepository.existsByLaptopIdAndActiveTrue(objectMapper.convertValue(wishlist, Laptop.class).getLaptopId())) {
+                throw new NotFound404Exception("Product Not Found");
+            }
+            if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(objectMapper.convertValue(wishlist, Laptop.class).getLaptopId(), ProductEnum.LAPTOP, getLoggedInUser())) {
+                wishlistProduct.setProduct(ProductEnum.LAPTOP);
+                wishlistProduct.setProductId(objectMapper.convertValue(wishlist, Laptop.class).getLaptopId());
+                wishlistProduct.setUser(getLoggedInUser());
+                wishlistProductRepository.save(wishlistProduct);
+                return wishlistProduct;
+            } else {
+                throw new Conflict409Exception("This Product Already Exists");
+            }
         } else if (objectMapper.convertValue(wishlist, AC.class).getAcId() != null) {
-            wishlistProduct.setProduct(ProductEnum.AC);
-            wishlistProduct.setProductId(objectMapper.convertValue(wishlist, AC.class).getAcId());
-            wishlistProduct.setUser(getLoggedInUser());
-            wishlistProductRepository.save(wishlistProduct);
-            return wishlistProduct;
+            if (!acRepository.existsByAcIdAndActiveTrue(objectMapper.convertValue(wishlist, AC.class).getAcId())) {
+                throw new NotFound404Exception("Product Not Found");
+            }
+            if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(objectMapper.convertValue(wishlist, AC.class).getAcId(), ProductEnum.AC, getLoggedInUser())) {
+                wishlistProduct.setProduct(ProductEnum.AC);
+                wishlistProduct.setProductId(objectMapper.convertValue(wishlist, AC.class).getAcId());
+                wishlistProduct.setUser(getLoggedInUser());
+                wishlistProductRepository.save(wishlistProduct);
+                return wishlistProduct;
+            } else {
+                throw new Conflict409Exception("This Product Already Exists");
+            }
         } else if (objectMapper.convertValue(wishlist, WashingMachine.class).getWashingMachineId() != null) {
-            wishlistProduct.setProduct(ProductEnum.WASHINGMACHINE);
-            wishlistProduct.setProductId(objectMapper.convertValue(wishlist, WashingMachine.class).getWashingMachineId());
-            wishlistProduct.setUser(getLoggedInUser());
-            wishlistProductRepository.save(wishlistProduct);
-            return wishlistProduct;
+            if (!washingMachineRepository.existsByWashingMachineIdAndActiveTrue(objectMapper.convertValue(wishlist, WashingMachine.class).getWashingMachineId())) {
+                throw new NotFound404Exception("Product Not Found");
+            }
+            if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(objectMapper.convertValue(wishlist, WashingMachine.class).getWashingMachineId(), ProductEnum.WASHINGMACHINE, getLoggedInUser())) {
+                wishlistProduct.setProduct(ProductEnum.WASHINGMACHINE);
+                wishlistProduct.setProductId(objectMapper.convertValue(wishlist, WashingMachine.class).getWashingMachineId());
+                wishlistProduct.setUser(getLoggedInUser());
+                wishlistProductRepository.save(wishlistProduct);
+                return wishlistProduct;
+            } else {
+                throw new Conflict409Exception("This Product Already Exists");
+            }
         } else if (objectMapper.convertValue(wishlist, Television.class).getTelevisionId() != null) {
-            wishlistProduct.setProduct(ProductEnum.TELEVISION);
-            wishlistProduct.setProductId(objectMapper.convertValue(wishlist, Television.class).getTelevisionId());
-            wishlistProduct.setUser(getLoggedInUser());
-            wishlistProductRepository.save(wishlistProduct);
-            return wishlistProduct;
+            if (!televisionRepository.existsByTelevisionIdAndActiveTrue(objectMapper.convertValue(wishlist, Television.class).getTelevisionId())) {
+                throw new NotFound404Exception("Product Not Found");
+            }
+            if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(objectMapper.convertValue(wishlist, Television.class).getTelevisionId(), ProductEnum.TELEVISION, getLoggedInUser())) {
+                wishlistProduct.setProduct(ProductEnum.TELEVISION);
+                wishlistProduct.setProductId(objectMapper.convertValue(wishlist, Television.class).getTelevisionId());
+                wishlistProduct.setUser(getLoggedInUser());
+                wishlistProductRepository.save(wishlistProduct);
+                return wishlistProduct;
+            } else {
+                throw new Conflict409Exception("This Product Already Exists");
+            }
         } else if (objectMapper.convertValue(wishlist, MobilePhone.class).getMobilePhoneId() != null) {
-            wishlistProduct.setProduct(ProductEnum.MOBILEPHONE);
-            wishlistProduct.setProductId(objectMapper.convertValue(wishlist, MobilePhone.class).getMobilePhoneId());
-            wishlistProduct.setUser(getLoggedInUser());
-            wishlistProductRepository.save(wishlistProduct);
-            return wishlistProduct;
+            if (!mobilePhoneRepository.existsByMobilePhoneIdAndActiveTrue(objectMapper.convertValue(wishlist, MobilePhone.class).getMobilePhoneId())) {
+                throw new NotFound404Exception("Product Not Found");
+            }
+            if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(objectMapper.convertValue(wishlist, MobilePhone.class).getMobilePhoneId(), ProductEnum.MOBILEPHONE, getLoggedInUser())) {
+                wishlistProduct.setProduct(ProductEnum.MOBILEPHONE);
+                wishlistProduct.setProductId(objectMapper.convertValue(wishlist, MobilePhone.class).getMobilePhoneId());
+                wishlistProduct.setUser(getLoggedInUser());
+                wishlistProductRepository.save(wishlistProduct);
+                return wishlistProduct;
+            } else {
+                throw new Conflict409Exception("This Product Already Exists");
+            }
         } else if (objectMapper.convertValue(wishlist, Refrigerator.class).getRefrigeratorId() != null) {
-            wishlistProduct.setProduct(ProductEnum.REFRIGERATOR);
-            wishlistProduct.setProductId(objectMapper.convertValue(wishlist, Refrigerator.class).getRefrigeratorId());
-            wishlistProduct.setUser(getLoggedInUser());
-            wishlistProductRepository.save(wishlistProduct);
-            return wishlistProduct;
+            if (!refrigeratorRepository.existsByRefrigeratorIdAndActiveTrue(objectMapper.convertValue(wishlist, Refrigerator.class).getRefrigeratorId())) {
+                throw new NotFound404Exception("Product Not Found");
+            }
+            if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(objectMapper.convertValue(wishlist, Refrigerator.class).getRefrigeratorId(), ProductEnum.REFRIGERATOR, getLoggedInUser())) {
+                wishlistProduct.setProduct(ProductEnum.REFRIGERATOR);
+                wishlistProduct.setProductId(objectMapper.convertValue(wishlist, Refrigerator.class).getRefrigeratorId());
+                wishlistProduct.setUser(getLoggedInUser());
+                wishlistProductRepository.save(wishlistProduct);
+                return wishlistProduct;
+            } else {
+                throw new Conflict409Exception("This Product Already Exists");
+            }
         }
         return new BadRequest400Exception("Something went wrong, Please try again!" + wishlist);
     }
@@ -113,81 +155,81 @@ public class WishlistProductService {
         Long id = objectMapper.readValue(urlSplit[urlSplit.length - 2], Long.class);
         switch (urlSplit[urlSplit.length - 1]) {
             case "getAcById":
-                if (!acRepository.existsByAcIdAndActiveTrue(id)){
+                if (!acRepository.existsByAcIdAndActiveTrue(id)) {
                     throw new NotFound404Exception("Product Not Found");
                 }
-                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id,ProductEnum.AC,getLoggedInUser())){
+                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id, ProductEnum.AC, getLoggedInUser())) {
                     wishlistProduct.setProduct(ProductEnum.AC);
                     wishlistProduct.setUser(getLoggedInUser());
                     wishlistProduct.setProductId(id);
                     wishlistProductRepository.save(wishlistProduct);
                     return wishlistProduct;
-                }else {
+                } else {
                     throw new Conflict409Exception("This Product Already Exists");
                 }
             case "getLaptopById":
-                if (!laptopRepository.existsByLaptopIdAndActiveTrue(id)){
+                if (!laptopRepository.existsByLaptopIdAndActiveTrue(id)) {
                     throw new NotFound404Exception("Product Not Found");
                 }
-                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id,ProductEnum.LAPTOP,getLoggedInUser())){
+                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id, ProductEnum.LAPTOP, getLoggedInUser())) {
                     wishlistProduct.setProduct(ProductEnum.LAPTOP);
                     wishlistProduct.setUser(getLoggedInUser());
                     wishlistProduct.setProductId(id);
                     wishlistProductRepository.save(wishlistProduct);
                     return wishlistProduct;
-                }else {
+                } else {
                     throw new Conflict409Exception("This Product Already Exists");
                 }
             case "getMobilePhoneById":
-                if (!mobilePhoneRepository.existsByMobilePhoneIdAndActiveTrue(id)){
+                if (!mobilePhoneRepository.existsByMobilePhoneIdAndActiveTrue(id)) {
                     throw new NotFound404Exception("Product Not Found");
                 }
-                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id,ProductEnum.MOBILEPHONE,getLoggedInUser())){
+                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id, ProductEnum.MOBILEPHONE, getLoggedInUser())) {
                     wishlistProduct.setProduct(ProductEnum.MOBILEPHONE);
                     wishlistProduct.setUser(getLoggedInUser());
                     wishlistProduct.setProductId(id);
                     wishlistProductRepository.save(wishlistProduct);
                     return wishlistProduct;
-                }else {
+                } else {
                     throw new Conflict409Exception("This Product Already Exists");
                 }
             case "getRefrigeratorById":
-                if (!refrigeratorRepository.existsByRefrigeratorIdAndActiveTrue(id)){
+                if (!refrigeratorRepository.existsByRefrigeratorIdAndActiveTrue(id)) {
                     throw new NotFound404Exception("Product Not Found");
                 }
-                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id,ProductEnum.REFRIGERATOR,getLoggedInUser())){
+                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id, ProductEnum.REFRIGERATOR, getLoggedInUser())) {
                     wishlistProduct.setProduct(ProductEnum.REFRIGERATOR);
                     wishlistProduct.setUser(getLoggedInUser());
                     wishlistProduct.setProductId(id);
                     wishlistProductRepository.save(wishlistProduct);
                     return wishlistProduct;
-                }else {
+                } else {
                     throw new Conflict409Exception("This Product Already Exists");
                 }
             case "getTelevisionById":
-                if (!televisionRepository.existsByTelevisionIdAndActiveTrue(id)){
+                if (!televisionRepository.existsByTelevisionIdAndActiveTrue(id)) {
                     throw new NotFound404Exception("Product Not Found");
                 }
-                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id,ProductEnum.TELEVISION,getLoggedInUser())){
+                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id, ProductEnum.TELEVISION, getLoggedInUser())) {
                     wishlistProduct.setProduct(ProductEnum.TELEVISION);
                     wishlistProduct.setUser(getLoggedInUser());
                     wishlistProduct.setProductId(id);
                     wishlistProductRepository.save(wishlistProduct);
                     return wishlistProduct;
-                }else {
+                } else {
                     throw new Conflict409Exception("This Product Already Exists");
                 }
             case "getWashingMachineById":
-                if (!washingMachineRepository.existsByWashingMachineIdAndActiveTrue(id)){
+                if (!washingMachineRepository.existsByWashingMachineIdAndActiveTrue(id)) {
                     throw new NotFound404Exception("Product Not Found");
                 }
-                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id,ProductEnum.WASHINGMACHINE,getLoggedInUser())){
+                if (!wishlistProductRepository.existsByProductIdAndProductAndUserAndDeletedFalse(id, ProductEnum.WASHINGMACHINE, getLoggedInUser())) {
                     wishlistProduct.setProduct(ProductEnum.WASHINGMACHINE);
                     wishlistProduct.setUser(getLoggedInUser());
                     wishlistProduct.setProductId(id);
                     wishlistProductRepository.save(wishlistProduct);
                     return wishlistProduct;
-                }else {
+                } else {
                     throw new Conflict409Exception("This Product Already Exists");
                 }
             default:
