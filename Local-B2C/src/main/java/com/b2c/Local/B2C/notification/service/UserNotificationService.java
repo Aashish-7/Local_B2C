57f31dcfc40aa5cf84a25a06108332f713dbdd3f
rishabh-ioanyt <rishabh.maurya@ioanyt.com>
2022-12-27 16:59:10 +0530

    UserNotificationService added

--- Local-B2C/src/main/java/com/b2c/Local/B2C/notification/service/UserNotificationService.java
+++ Local-B2C/src/main/java/com/b2c/Local/B2C/notification/service/UserNotificationService.java
@@ -1,5 +1,6 @@
 package com.b2c.Local.B2C.notification.service;
 
+import com.b2c.Local.B2C.common.dao.WishlistProductRepository;
 import com.b2c.Local.B2C.notification.dao.UserNotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,17 @@ public class UserNotificationService {
 
     UserNotificationRepository userNotificationRepository;
 
+    WishlistProductRepository wishlistProductRepository;
+
     @Autowired
-    public UserNotificationService(UserNotificationRepository userNotificationRepository) {
+    public UserNotificationService(UserNotificationRepository userNotificationRepository, WishlistProductRepository wishlistProductRepository) {
         this.userNotificationRepository = userNotificationRepository;
+        this.wishlistProductRepository = wishlistProductRepository;
     }
+
+    public void getWishlistProduct(){
+
+    }
+
+
 }
