c347fb4632f22f61ae69b824062cacdf50bce0dc
rishabh-ioanyt <rishabh.maurya@ioanyt.com>
2023-02-07 19:01:36 +0530

    one Client Oauth2 added

---
+++ Local-B2C/src/main/java/com/b2c/Local/B2C/config/CustomOauth2Config.java
@@ -0,0 +1,50 @@
+package com.b2c.Local.B2C.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+
+@Configuration
+public class CustomOauth2Config {
+
+    @Value("${spring.security.oauth2.client.registration.github.client-secret}")
+    String gitSecret;
+    @Value("${spring.security.oauth2.client.registration.github.client-id}")
+    String gitClientId;
+
+
+    @Bean
+    public ClientRegistrationRepository clientRepository() {
+        ClientRegistration githubRegistration =
+                CommonOAuth2Provider.GITHUB.getBuilder("github").clientId(gitClientId)
+                        .clientSecret(gitSecret)
+                        .build();
+        return new InMemoryClientRegistrationRepository(githubRegistration,
+                googleClientRegistration());
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("authServer")
+                .clientId("client")
+                .clientSecret("secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://authserver:8080/login/oauth2/code/authServer")
+                .scope("read")
+                .authorizationUri("http://authserver:8000/oauth2/authorize")
+                .tokenUri("http://authserver:8000/oauth2/token")
+                .userInfoUri("http://authserver:8000/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("http://authserver:8000/oauth2/jwks")
+                .clientName("local-b2c")
+                .build();
+    }
+
+}
